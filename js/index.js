e531473f2bebb42b30876dce6b78ebfd2852c893
BenoitGaudieri <benalek@gmail.com>
2020-04-30 21:17:22 +0200

    single day display

--- js/index.js
+++ js/index.js
@@ -1,13 +1,3 @@
-// Descrizione
-// Creare un calendario dinamico con le festività. Partiamo dal gennaio 2018 dando la possibilità di cambiare mese, gestendo il caso in cui l’API non possa ritornare festività. Il calendario partirà da gennaio 2018 e si concluderà a dicembre 2018 (unici dati disponibili sull’API).
-// Ogni volta che cambio mese dovrò:
-// Controllare se il mese è valido (per ovviare al problema che l’API non carichi holiday non del 2018)
-// Controllare quanti giorni ha il mese scelto formando così una lista
-// Chiedere all’api quali sono le festività per il mese scelto
-// Evidenziare le festività nella lista
-// BONUS OPZIONALE:
-// Trasformare la lista precedente in un vero e proprio calendario, generando una griglia che segua l’andamento dei giorni di un mese a scelta, evidenziando le festività.`
-// Creare dei bottoni che permettano di spostarsi di mese in mese, rigenerando ogni volta la griglia e le festività associate
 $(document).ready(function () {
     /**
      * Setup
@@ -145,7 +135,7 @@ function genCalendar(baseMonth, template) {
     // get holidays
     printHoliday(baseMonth);
 
-    // TODO: add baseMonth controller that turns off the buttons
+    week(baseMonth);
 }
 
 /**
@@ -179,4 +169,12 @@ function changeMonth(baseMonth, template, operation) {
 function changeLocale(baseMonth, locale = "en") {
     baseMonth.locale(locale);
     moment.locale(locale);
+}
+
+function week(baseMonth) {
+    var week = moment.weekdaysShort();
+    $(".weeks-label").html("");
+    for (let i = 0; i < week.length; i++) {
+        $(".weeks-label").append("<li class='wday'>" + week[i] + "</li>");
+    }
 }
